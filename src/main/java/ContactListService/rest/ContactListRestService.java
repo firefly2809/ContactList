41b3fd1d7f7dc7e2a7c9db6e6107bf3eb94c50ba
16712917 <Podgorny.S.Al@sberbank.ru>
2018-10-17 19:02:22 +0400

    ...

---
+++ src/main/java/ContactListService/rest/ContactListRestService.java
@@ -0,0 +1,19 @@
+package ContactListService.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path("/rest")
+public class ContactListRestService {
+
+    @Path("/getContact/{name}")
+    @GET
+    @Produces("application/json")
+    public String getContact(@PathParam("name") String name) {
+        //Описание метода по получению контакта
+        return String.format("{\n%s: 12345678}", name);
+    }
+
+}
