1835be05db9d6ae81e58e6defe2b5d081660fb6a
16712917 <Podgorny.S.Al@sberbank.ru>
2018-10-18 11:26:26 +0400

    ...

--- src/main/java/ContactListService/rest/ContactListRestService.java
+++ src/main/java/ContactListService/rest/ContactListRestService.java
@@ -5,7 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-@Path("/rest")
+@Path("/ContactList")
 public class ContactListRestService {
 
     @Path("/getContact/{name}")
